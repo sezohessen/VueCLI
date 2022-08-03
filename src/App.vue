6c0ec9b319f03bc25e90a1d530028904d4bddcf8
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-03 16:26:40 +0200

    Vue Props

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,28 @@
   <h1>{{title}}</h1>
   <input type="text" ref="name">
   <button @click="handleClick"> Click Me</button>
-  <Modal/>
-  <Modal/>
+  <hr>
+  <Modal :header="header" text="This is dark theme" :age="age" theme="Dark"/>
+  <hr>
+  <Modal :header="header" text="This div not using theme" :age="age" />
 </template>
 
 <script>
 /* import HelloWorld from './components/HelloWorld.vue' */
-/* Props Make Component More Resuable */
+/* 
+  Props Make Component More Resuable and Dynmaic
+  Each time you use the modal you can pass different content 
+  into it as  <<Prop>>  from it's parent component
+*/
 import Modal from './components/Modal.vue'
 export default {
   name: 'App',
   components:{Modal},
   data() {
     return {
-      title:'Welcome Sezo'
+      title:'Welcome Sezo',
+      header:'Hello From Root',
+      age:23
     }
   },
   methods: {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,36 @@
 <template>
     <div class="backdrop">
-        <div class="modal">
-            <h1>Hello From component</h1>
+        <div class="modal" :class="{theme_dark: theme==='Dark'}">
+            <h1>{{ header ? header: 'Not Found Title' }}</h1>
+            <p>{{ text }}</p>
+            <span>Your age is {{age}}</span>
+            <div v-if="theme!=='Dark'">
+                <button @click="ChangeTheme">Click To Choose Dark Theme</button>
+            </div>
         </div>
     </div>
 </template>
-
+<script>
+export default {/* Get Data from parent component */
+    props:['header','text','age','theme'],
+    methods: {
+        ChangeTheme(){
+            this.theme = 'Dark'
+        }
+    },
+}
+</script>
 <style scoped>
+    .backdrop{
+        color: #000000;
+    }
     h1{
         color: red;
         display: inline;
         border-bottom: 1px solid #ccc;
+    }
+    .theme_dark{
+        background-color: rgb(12, 8, 8);
+        color: #ffffff;
     }
 </style>
