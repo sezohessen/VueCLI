e28d70e4fc74f453fe68ab223d66046a0eef4f78
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-03 22:38:22 +0200

    Starting Game

---
+++ src/Timer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="margin:20px;height:500px">
+    <h1>Sezo Reaction Time</h1>
+    <button @click="Start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay"/>
+  </div>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+export default {
+  name: 'Timer',
+  components:{Block},
+  data() {
+    return {
+      isPlaying:false,
+      delay:null
+    }
+  },
+  methods: {
+    Start(){
+      this.delay = 2000 + Math.random() * 5000//2~7
+      this.isPlaying = true
+    }
+  },
+}
+</script>
+
+<style>
+  #reaction_time {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import './assets/global.css'
 import App from './App.vue'
+import Timer from './Timer.vue'
 createApp(App).mount('#app')
+createApp(Timer).mount('#reaction_time')
