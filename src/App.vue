afefa82f2772db9e094fe45677189a9f506cdb2d
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-03 20:51:42 +0200

    Vue using Teleport

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,21 @@
   <button @click="ToggleModal">Open Modal</button>
 
   <p><button @click.right="ToggleModal">Open Modal (right click)</button></p>
-  <Modal :header="header" text="This is dark theme" :age="age" theme="Dark" :ShowModal="ShowModal" @close="ToggleModal">
-    <template v-slot:links><!-- Specific structure for the component -->
-      <ul>
-        <li><a href="#">Sign up</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">More Info</a></li>
-      </ul>
-    </template>
-    <h1>Welcome Hussien This is Vue3 Tutorial</h1><!-- Default Content slot -->
-    <p>I hope you are doing great :) </p>
-  </modal>
+    <Modal :header="header" text="This is dark theme" :age="age" theme="Dark" :ShowModal="ShowModal" @close="ToggleModal">
+      <template v-slot:links><!-- Specific structure for the component -->
+        <ul>
+          <li><a href="#">Sign up</a></li>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">More Info</a></li>
+        </ul>
+      </template>
+      <h1>Welcome Hussien This is Vue3 Tutorial</h1><!-- Default Content slot -->
+      <p>I hope you are doing great :) </p>
+    </Modal>
   <hr>
-  <Modal :header="header" text="This div not using theme" :age="age" />
+  <teleport to='.modals' >
+    <Modal :header="header" text="This div not using theme, (USing teleport)" :age="age" />
+  </teleport>
 </template>
 
 <script>
@@ -54,7 +56,7 @@ export default {
 </script>
 
 <style>
-#app {
+#app,.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
