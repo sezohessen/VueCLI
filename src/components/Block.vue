12c441f6e33ffde058afd524009f782bac9c29af
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-03 23:03:26 +0200

    Vue Lifecycle Hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock">
       Click Me
   </div>
 </template>
@@ -7,6 +7,25 @@
 <script>
 export default {
     props:['delay'],
+    data() {
+        return {
+            showBlock:false,
+        }
+    },
+    /* Vue Life Cycle Hooks */
+    mounted() {
+        console.log('Component Mounted')
+        setTimeout(()=>{
+            this.showBlock = true
+            console.log(this.delay)
+        },this.delay)
+    },
+    updated() {
+        console.log('Component updated')
+    },
+    unmounted() {/* When using vue routing from page to page, the component will unmounted when we navigate from page */
+        console.log('Component unmounted')
+    },
 }
 </script>
 
