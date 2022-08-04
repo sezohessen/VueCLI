823adddf2bcdc8754b2d9fb0d74fe62c176742ed
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-04 02:08:49 +0200

    Form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './assets/global.css'
 import App from './App.vue'
+import Form from './Form.vue'
 import Timer from './Timer.vue'
 createApp(App).mount('#app')
 createApp(Timer).mount('#reaction_time')
+createApp(Form).mount('#Form')
