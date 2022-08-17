466382bbdf4a1706f396ff7cddf55839d3a9d61b
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-17 20:20:24 +0200

    Add skills in from

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,12 +1,34 @@
 <template>
   <form>
       <label>Email:</label>
-      <input type="email" name="" id="" required v-model="email">
+      <input type="email" name="" id="" required v-model="email" >
       <label>Password:</label>
       <input type="password" name="" id="" required v-model="password">
+      <label >Role:</label>
+      <select v-model="role">
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+      <div class="terms">
+          <input type="checkbox" required v-model="terms">
+          <label>Accept terms and conditions</label>
+      </div>
+      <label>Skills: <span>(Press enter to add skill)</span> </label>
+      <input type="text"  v-model="tempSkill" @keyup="addSkill">
+      <div v-for="skill in skills" :key="skill" class="pill">
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
+      </div>
+      <!-- 
+        V-model tracking the value of attribute and update it to the input
+        v-model is two way data binding:
+        1- one way from data to the templete
+        2- the other way from templete to update data
+      -->
   </form>
   <p>Email: {{email}}</p>
   <p>Password: {{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>terms: accepted : {{terms}}</p>
 </template>
 
 <script>
@@ -14,7 +36,31 @@ export default {
     data() {
         return {
             email:'',
-            password:''
+            password:'',
+            role:'',
+            terms:false,
+            tempSkill:'',
+            skills:[]
+        }
+    },
+    methods: {
+        addSkill(e){
+            if((e.key === 'Enter')&& this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter(item => {
+                return skill !== item
+            })
+            /* 
+            Filter method cycle through an array and fires funtion for each item
+            if it return true for an item in array then we keep it in the array if we return false
+            we remove it from array 
+             */
         }
     },
 }
@@ -46,5 +92,24 @@ input, select {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 </style>
